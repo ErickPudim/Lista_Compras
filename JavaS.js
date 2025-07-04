4574ed9707c853787f20592035125cddda6418b4
Anderson Antunes Soares Filho <filhoandersonsoares@gmail.com>
2025-07-04 10:48:13 -0300

    versão com comentários

--- JavaS.js
+++ JavaS.js
@@ -1,3 +1,5 @@
+//localstorage.getitem 1
+//coleta o valor dos produtos em forma já pronta em listas, com o valor riscado ou n
 var products = JSON.parse(localStorage.getItem("products")) || []
 
 function Show(){
@@ -5,6 +7,10 @@ function Show(){
     document.getElementById("List").innerHTML = ""
     for(let i = 0; i < product.length; i++ ){
         document.getElementById("List").innerHTML += 
+        //eventlistener 3
+        //verifica quando o botão de riscar o nome do produto é pressionado
+        //eventlistener 4
+        //verifica quando o botão de remover o produto é pressionado
             `<div id="${product[i][0]}" class="Item">
                 <p class="Main">${product[i][0]}</p>
                 <div class="Buttons">
@@ -30,6 +36,8 @@ function add(product){
     }
     if(exists == false){
         products[products.length] = [product,"none"]
+        //localstorage.setitem 1
+        //atualiza o valor armazenado, adicionando o produto adicionado
         localStorage.setItem("products", JSON.stringify(products))
         Show()
     }
@@ -45,6 +53,8 @@ function remover(button){
         var div = button.parentNode.parentNode
         if (product == div){
             products.splice(i,1)
+            //localstorage.setitem 2
+            //atualiza o valor armazenado, removendo o produto removido
             localStorage.setItem("products", JSON.stringify(products))
             Show()
         }
@@ -70,6 +80,8 @@ function riscar(button){
             else{
                 products[i][1] = "none"
             }
+            //localstorage.setitem 3 
+            //atualiza o valor armazenado, valor de se o nome do produto está riscado ou não
             localStorage.setItem("products", JSON.stringify(products))
             Show()
         }
@@ -79,17 +91,25 @@ function riscar(button){
 function desriscartudo(){
     for(let i = 0; i<products.length; i++ ){
         products[i][1] = "none"
+        //localstorage.setitem 4
+        //atualiza o valor armazenado, tirando o risco de todos os produtos, um por um
         localStorage.setItem("products", JSON.stringify(products))
     }
     Show()
 }
 
 function deleteall(){
+    //localstorage.removeitem 1
+    //remove todos os produtos
     localStorage.removeItem("products")
+    //localstorage.getitem 2
+    //atualiza o valor removido
     products = JSON.parse(localStorage.getItem("products")) || []
     Show()
 }
 
+//eventlistener 5
+//verifica se o botão de adicionar produto foi clicado
 document.getElementById("Send").addEventListener('click',colect)
 Show()
 
